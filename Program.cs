948f2b5fa5d96b18c1c7f9f9864154ad837a1e3e
witkullapob pumimaha <eearth49@gmail.com>
2023-03-13 17:13:41 +0700

    witkullapob

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿class Program {
+    static void Main(string[] args) {
+        
+        Console.Write("Input Day of week : ");
+        int a = int.Parse(Console.ReadLine());
+        
+        Console.Write("Input Time of day : ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Input Menu : ");
+        string c = (Console.ReadLine());       
+        
+        if (a >= 1 && a <= 7){
+        int monday = a / 1;
+        int tuesday = a / 1;
+        int wednesday = a / 1;
+        int thursday = a / 1;
+        int friday = a / 1;
+        int saturday = a / 1;
+        int sunday = a / 1;
+
+        if (b >= 8 && b <= 18){
+            
+        int eightOclock = b / 1;
+        int nineOclock = b / 1;
+        int tenOclock = b / 1;
+        int elevenOclock = b / 1;
+        int twelveOclock = b / 1;
+        int thirteenOclock = b / 1;
+        int fourteenOclock = b / 1;
+        int fifteenOclock = b / 1;
+        int sixteenOclock = b / 1;
+        int seventeenOclock = b / 1;
+        int eighteenOclock = b / 1;
+            
+        }
+
+        }
+        if(c == "Breakfast set"){
+            Console.WriteLine(" Sorry,Out of order");
+        }
+
+        if(c == "Weekend set"){
+            Console.WriteLine(" Sorry,Out of order ");
+        }
+
+        if(c == "Coffee"){
+            Console.WriteLine(" Sorry,Out of order ");
+        }
+        
+
+
+        }
+        
+
+
+
+
+        }
+
+
+
+
+
+    
+
+    
